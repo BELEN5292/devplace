3e2596923b77af308b47abc1ff7859597ba7aeef
Jesuan Dip <jesuan_2@hotmail.com>
2021-11-01 21:19:34 -0300

    primera version

---
+++ api devplace/Domain/Models/ArticuloBlog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api_devplace.Repository;
+
+namespace api.devplace.Domain.Models
+{
+    public class ArticuloBlog : EntityBase
+    {
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Contenido { get; set; }
+        public virtual ICollection<ComentarioBlog> Comments { get; set; } = new List<ComentarioBlog>();
+    }
+}
---
+++ api devplace/Domain/Models/ComentarioBlog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api_devplace.Repository;
+
+namespace api.devplace.Domain.Models
+{
+    public class ComentarioBlog : EntityBase
+    {
+       
+        public string Texto { get; set; }
+      
+        public ArticuloBlog Articulo { get; set; }
+
+        
+        public int ArticuloId { get; set; }
+        
+
+
+    }
+}
+
